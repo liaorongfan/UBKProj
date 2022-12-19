3edaed2a47a7ef7e1168d0603d04aae63a6dfc5f
liaorongfan <15670381505@163.com>
2022-12-20 01:26:59 +0800

    MLP acc 0.8044

--- src/modeling/network/mlp.py
+++ src/modeling/network/mlp.py
@@ -7,15 +7,22 @@ class MLP(nn.Module):
     def __init__(self,input_dim=194, output_dim=3):
         super().__init__()
         self.inp_layer = nn.Linear(input_dim, 256)
-        self.mid_layer = nn.Linear(256, 512)
-        self.mid_relu = nn.ReLU()
+        self.relu_1 = nn.LeakyReLU()
+
+        self.layer_2 = nn.Linear(256, 512)
+        self.relu_2 = nn.LeakyReLU()
+
+        self.layer_3 = nn.Linear(512, 512)
+        self.relu_3 = nn.LeakyReLU()
+
+        self.drop_out = nn.Dropout(0.2)
         self.out_layer = nn.Linear(512, output_dim)
-        self.relu = nn.ReLU()
 
     def forward(self, x):
-        x = self.inp_layer(x)
-        x = self.relu(x)
-        x = self.mid_relu(self.mid_layer(x))
+        x = self.relu_1(self.inp_layer(x))
+        x = self.relu_2(self.layer_2(x))
+        x = self.relu_3(self.layer_3(x))
+        x = self.drop_out(x)
         x = self.out_layer(x)
         return x
 
